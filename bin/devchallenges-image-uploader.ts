eb0d379387103dc832a09761f02d610c90400007
surumebeer <06shuji09@gmail.com>
2020-12-02 03:30:53 +0900

    add: prettier

--- bin/devchallenges-image-uploader.ts
+++ bin/devchallenges-image-uploader.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { DevchallengesImageUploaderBackendStack } from '../lib/devchallenges-image-uploader-backend-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { DevchallengesImageUploaderBackendStack } from "../lib/devchallenges-image-uploader-backend-stack";
 
 const app = new cdk.App();
-new DevchallengesImageUploaderBackendStack(app, 'DevchallengesImageUploaderBackendStack');
+new DevchallengesImageUploaderBackendStack(
+  app,
+  "DevchallengesImageUploaderBackendStack"
+);
--- test/devchallenges-image-uploader.test.ts
+++ test/devchallenges-image-uploader.test.ts
@@ -1,13 +1,25 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as DevchallengesImageUploader from '../lib/devchallenges-image-uploader-backend-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as DevchallengesImageUploader from "../lib/devchallenges-image-uploader-backend-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new DevchallengesImageUploader.DevchallengesImageUploaderBackendStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new DevchallengesImageUploader.DevchallengesImageUploaderBackendStack(
+    app,
+    "MyTestStack"
+  );
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
